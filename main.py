91e8789e9d3c9ffb301a8a9191951d8529339792
simgeagkaya <agkayasimge.50@gmail.com>
2022-10-20 18:37:29 +0300

    algorithm questions answer

---
+++ main.py
@@ -0,0 +1,16 @@
+print('merhaba dünya')
+isim =input('merhaba isminizi giriniz:')
+print(isim)
+sayi1=int(input('birinci sayıyı girin :'))
+sayi2=int(input('ikinci sayıyı girin'))
+topla=(sayi1+sayi2)
+print(':toplam ',topla)
+
+sayi1=int(input('birinci sayıyı girin :'))
+sayi2=int(input('ikinci sayıyı girin'))
+orta=((sayi1+sayi2)/2)
+print('Ortalama: ',orta)
+
+
+
+
